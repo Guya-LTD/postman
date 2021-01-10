7cea4f68d47ef44799c50cc95dc69b213e56bd99
Simonbelete <simonbelete@gmail.com>
2021-01-10 09:38:40 +0300

    Products runner

---
+++ newman/products.js
@@ -0,0 +1,22 @@
+const newman = require('newman');
+
+// Vars
+const COLLECTION_DIR = __dirname + '/../collections';
+const FAKER_DIR = __dirname + '/../../faker';
+const ENVIRONMENT_DIR = __dirname + '/../environments';
+
+var collections = COLLECTION_DIR + '/' + 'Guya-LTD.postman_collection.json' //require('Guya LTD.postman_collection.json');
+var environments = ENVIRONMENT_DIR + '/' + 'Guya-LTD Environment.postman_environment.json' //require('Guya LTD Environment.postman_environment.json');
+
+// 2. roles
+newman.run({
+    collection: collections,
+    reporters: ['cli', 'htmlextra', 'progress', 'emojitrain'],
+    reporter : { htmlextra : { export : './results/categories-post.html'} },
+    folder: "products - post",
+    environment: environments,
+    iterationData: FAKER_DIR + '/' + 'products.json'
+}, function (err) {
+    if (err) { throw err; }
+    console.log('\`\/api\/v1\/products\` run complete!');
+});
